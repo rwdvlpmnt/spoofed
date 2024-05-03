775ac285f795e34e20649346c2b8a57632943b88
rwdvlpmnt <rwdvlpmnt@gmail.com>
2024-05-03 01:47:51 +0000

    deployent updates

--- phoneApp/deployment.py
+++ phoneApp/deployment.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTION_STRING']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
