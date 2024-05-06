3e5b86d41b399ecf001c103b6f38f39ea841449a
rwdvlpmnt <rwdvlpmnt@gmail.com>
2024-05-06 20:49:19 +0000

    urls.py edit

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
