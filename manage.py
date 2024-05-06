79a00946cfc7e34d5e593de991b73b29873079a2
rwdvlpmnt <rwdvlpmnt@gmail.com>
2024-05-06 18:06:03 +0000

    manage.py edits

--- manage.py
+++ manage.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+def get_current_host():
+    if 'HTTP_HOST' in os.environ:
+        return os.environ['HTTP_HOST']
+    elif 'SERVER_NAME' in os.environ:
+        return os.environ['SERVER_NAME']
+    else:
+        return None
 
-def main():
-    """Run administrative tasks."""
-    settings_module = 'phoneApp.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'phoneApp.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+ALLOWED_HOSTS = ['spoofed.azurewebsites.net', 'youbeenspoofed.com', 'www.spoofed.azurewebsites.net', 'www.youbeenspoofed.com']
+current_host = get_current_host()
 
+if current_host and any(host in current_host for host in ALLOWED_HOSTS):
+    settings_module = 'phoneApp.production'
+else:
+    settings_module = 'phoneApp.settings'
 
-if __name__ == '__main__':
-    main()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+try:
+    from django.core.management import execute_from_command_line
+except ImportError as exc:
+    raise ImportError(
+        "Couldn't import Django. Are you sure it's installed and "
+        "available on your PYTHONPATH environment variable? Did you "
+        "forget to activate a virtual environment?"
+    ) from exc
+
+execute_from_command_line(sys.argv)
